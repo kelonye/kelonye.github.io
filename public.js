601e0fa30de3a9bdd08527efca9169d941944c9b
Mitchel Kelonye <kelonyemitchel@gmail.com>
2017-12-11 10:29:13 +0100

    rm past portfolio

--- public.js
+++ public.js
@@ -1,31 +1,28 @@
-// bind
-var fore = document.getElementById('fore');
-var imgs = document.querySelectorAll('ol img');
-var n = imgs.length;
-var img;
-while (n--){
-  img = imgs[n];
-  img.onclick = onclick.bind(img);
-}
-imgs[0].click();
+// // projects
+// var fore = document.getElementById('fore');
+// var imgs = document.querySelectorAll('ol img');
+// var n = imgs.length;
+// var img;
+// while (n--){
+//   img = imgs[n];
+//   img.onclick = function(e){
+//     var href = e.target.dataset.href;
+//     var src = e.target.src;
+//     var html = '';
+//     html += '<a href="'+href+'" target="_blank">';
+//     html += '<img src="'+src+'" width="940" height="450"/>';
+//     html += '</a>';
+//     fore.innerHTML = html;
+//     for (var i=0; i<imgs.length; i++){
+//       imgs[i].className = 'image--blurred';
+//     }
+//     this.className = 'image--visible';
+//   }
+// }
+// imgs[0].click();
 
 // &copy;
 var now = new Date;
 var year = now.getUTCFullYear();
 var el = document.getElementById('year');
 el.innerHTML = year;
-
-// onlick img
-function onclick(e){
-  var href = e.target.dataset.href;
-  var src = e.target.src;
-  var html = '';
-  html += '<a href="'+href+'" target="_blank">';
-  html += '<img src="'+src+'" width="940" height="450"/>';
-  html += '</a>';
-  fore.innerHTML = html;
-  for (var i=0; i<imgs.length; i++){
-    imgs[i].className = 'image--blurred';
-  }
-  this.className = 'image--visible';
-}
